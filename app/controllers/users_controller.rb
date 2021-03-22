99d7bde3613ff1426ccaa36be84c286f22b3444d
aoki.sae <muzyunkisa@yahoo.co.jp>
2021-03-22 08:57:00 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+ def create
     @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-　end
+ end
   
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
 end
